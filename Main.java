862082b4042ce11014576c5f81e4883ecdab9d0b
Fuzes Marcell <fuzes.marcell@gmail.com>
2025-04-02 21:55:53 +0200

    Remove TODO

--- Main.java
+++ Main.java
@@ -374,8 +374,6 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-        // TODO: Handle all characters (escape characters)
-
         BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
 
         while (true) {
